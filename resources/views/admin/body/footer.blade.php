e17e640ab6cdc558743e0935ede73fa52a35a9ec
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-04-07 16:03:35 +0300

    customize dashboard

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -5,9 +5,9 @@
 			<a class="nav-link" href="javascript:void(0)">FAQ</a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link" href="#">Purchase Now</a>
+			<a class="nav-link" href="#">Contact Now</a>
 		  </li>
 		</ul>
     </div>
-	  &copy; 2020 <a href="#">Psd to Html Expert</a>. All Rights Reserved.
+	  &copy; 2022 <a href="#">Pro-Ecommerce</a>. All Rights Reserved.
   </footer>
