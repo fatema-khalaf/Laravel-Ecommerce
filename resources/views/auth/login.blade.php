3761e9f3534ee6026adef624813232be83513e53
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-04-09 17:25:36 +0300

    User Register templet & functionality setup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,22 +20,24 @@
                 <div class="col-md-6 col-sm-6 sign-in">
                     <h4 class="">Sign in</h4>
                     <p class="">Hello, Welcome to your account.</p>
+
                     <div class="social-sign-in outer-top-xs">
                         <a href="#" class="facebook-sign-in"><i class="fa fa-facebook"></i> Sign In with Facebook</a>
                         <a href="#" class="twitter-sign-in"><i class="fa fa-twitter"></i> Sign In with Twitter</a>
                     </div>
                     <form class="register-form outer-top-xs" role="form" method="POST"
                         action="{{ isset($guard) ? url($guard.'/login') : route('login') }}">
+
                         @csrf
                         <div class="form-group">
                             <label class="info-title" for="exampleInputEmail1">Email Address <span>*</span></label>
                             <input id="email" type="email" name="email"
-                                class="form-control unicase-form-control text-input" id="exampleInputEmail1">
+                                class="form-control unicase-form-control text-input">
                         </div>
                         <div class="form-group">
                             <label class="info-title" for="exampleInputPassword1">Password <span>*</span></label>
                             <input id="password" name="password" type="password"
-                                class="form-control unicase-form-control text-input" id="exampleInputPassword1">
+                                class="form-control unicase-form-control text-input">
                         </div>
                         <div class="radio outer-xs">
                             <label>
@@ -54,31 +56,48 @@
                 <div class="col-md-6 col-sm-6 create-new-account">
                     <h4 class="checkout-subtitle">Create a new account</h4>
                     <p class="text title-tag-line">Create your new account.</p>
-                    <form class="register-form outer-top-xs" role="form">
-                        <div class="form-group">
-                            <label class="info-title" for="exampleInputEmail2">Email Address <span>*</span></label>
-                            <input type="email" class="form-control unicase-form-control text-input"
-                                id="exampleInputEmail2">
-                        </div>
+
+                    <form class="register-form outer-top-xs" role="form" method="POST" action="{{ route('register') }}">
+                        @csrf
                         <div class="form-group">
                             <label class="info-title" for="exampleInputEmail1">Name <span>*</span></label>
-                            <input type="email" class="form-control unicase-form-control text-input"
-                                id="exampleInputEmail1">
+                            <input id="name" name="name" type="text"
+                                class="form-control unicase-form-control text-input">
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">{{$message}}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label class="info-title" for="exampleInputEmail2">Email Address <span>*</span></label>
+                            <input id="email" name="email" type="email"
+                                class="form-control unicase-form-control text-input" id="exampleInputEmail2">
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">{{$message}}</span>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label class="info-title" for="exampleInputEmail1">Phone Number <span>*</span></label>
-                            <input type="email" class="form-control unicase-form-control text-input"
-                                id="exampleInputEmail1">
+                            <input id="phone" name="phone" type="text"
+                                class="form-control unicase-form-control text-input">
+                            @error('phone')
+                            <span class="invalid-feedback" role="alert">{{$message}}</span>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label class="info-title" for="exampleInputEmail1">Password <span>*</span></label>
-                            <input type="email" class="form-control unicase-form-control text-input"
-                                id="exampleInputEmail1">
+                            <input id="password" name="password" type="password"
+                                class="form-control unicase-form-control text-input">
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">{{$message}}</span>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label class="info-title" for="exampleInputEmail1">Confirm Password <span>*</span></label>
-                            <input type="email" class="form-control unicase-form-control text-input"
-                                id="exampleInputEmail1">
+                            <input id="password_confirmation" name="password_confirmation" type="password"
+                                class="form-control unicase-form-control text-input">
+                            @error('password_confirmation')
+                            <span class="invalid-feedback" role="alert">{{$message}}</span>
+                            @enderror
                         </div>
                         <button type="submit" class="btn-upper btn btn-primary checkout-page-button">Sign Up</button>
                     </form>
@@ -90,54 +109,3 @@
     @include('frontend.body.brand')
 </div><!-- /.body-content -->
 @endsection
-
-{{-- <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
-        <x-jet-validation-errors class="mb-4" />
-
-        @if (session('status'))
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ session('status') }}
-        </div>
-        @endif
-
-        <form method="POST" action="{{ isset($guard) ? url($guard.'/login') : route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                    required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                    autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout> --}}
