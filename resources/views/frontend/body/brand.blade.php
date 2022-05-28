91c0b2f673e38842947fcf2771201101faf743f5
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-05-28 14:27:12 +0300

    Add brands section

--- resources/views/frontend/body/brand.blade.php
+++ resources/views/frontend/body/brand.blade.php
@@ -1,57 +1,17 @@
+@php
+$brands = App\Models\Brand::orderBy('id', 'DESC')->limit(6)->get();
+@endphp
+
 <div id="brands-carousel" class="logo-slider wow fadeInUp">
     <div class="logo-slider-inner">
         <div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
-            <div class="item m-t-15"> <a href="#" class="image"> <img
-                        data-echo="{{asset('frontend/assets/images/brands/brand1.png')}}" src="assets/images/blank.gif"
-                        alt="">
+            @foreach ($brands as $item)
+
+            <div class="item m-t-15"> <a href="#" class="image">
+                    <img style=" width: 120px; height: 120px;" data-echo="{{asset($item->brand_image)}}"
+                        src="{{asset('frontend/assets/images/blank.gif')}}" alt="{{asset($item->brand_name_en)}}">
                 </a> </div>
-            <!--/.item-->
-
-            <div class="item m-t-10"> <a href="#" class="image"> <img
-                        data-echo="{{asset('frontend/assets/images/brands/brand2.png')}}"
-                        src="{{asset('frontend/assets/images/blank.gif')}}" alt="">
-                </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img
-                        data-echo="{{asset('frontend/assets/images/brands/brand3.png')}}"
-                        src="{{asset('frontend/assets/images/blank.gif')}}" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img
-                        data-echo="{{asset('frontend/assets/images/brands/brand4.png')}}"
-                        src="{{asset('frontend/assets/images/blank.gif')}}" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img
-                        data-echo="{{asset('frontend/assets/images/brands/brand5.png')}}"
-                        src="{{asset('frontend/assets/images/blank.gif')}}" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img
-                        data-echo="{{asset('frontend/assets/images/brands/brand6.png')}}"
-                        src="{{asset('frontend/assets/images/blank.gif')}}" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img
-                        data-echo="{{asset('frontend/assets/images/brands/brand2.png')}}"
-                        src="{{asset('frontend/assets/images/blank.gif')}}" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img
-                        data-echo="{{asset('frontend/assets/images/brands/brand4.png')}}"
-                        src="{{asset('frontend/assets/images/blank.gif')}}" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img
-                        data-echo="{{asset('frontend/assets/images/brands/brand1.png')}}"
-                        src="{{asset('frontend/assets/images/blank.gif')}}" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img
-                        data-echo="{{asset('frontend/assets/images/brands/brand5.png')}}"
-                        src="{{asset('frontend/assets/images/blank.gif')}}" alt=""> </a> </div>
-            <!--/.item-->
+            @endforeach
         </div>
         <!-- /.owl-carousel #logo-slider -->
     </div>
